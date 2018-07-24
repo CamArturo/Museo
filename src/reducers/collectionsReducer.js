b699558ac4449ee993b50a74472cebbeec057743
Helen Dechat <hdechat@gmail.com>
2018-07-24 13:12:30 -0600

    Add collections reducer

---
+++ src/reducers/collectionsReducer.js
@@ -0,0 +1,18 @@
+initialState = {
+  collections: [],
+  collectionsIsLoading: false,
+  collectionsHasErrored: ''
+}
+
+export default const collectionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_COLLECTIONS':
+      return {...state, collections: action.collections};
+    case 'COLLECTIONS_IS_LOADING':
+      return {...state, collectionsIsLoading: action.bool};
+    case 'COLLECTIONS_HAS_ERRORED':
+      return {...state, collectionsHasErrored: action.error}
+    default:
+      return state;
+  }
+}
