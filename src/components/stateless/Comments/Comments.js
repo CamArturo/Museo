cf96f90d6632a45d4564b879f5becbff3c72e089
Helen Dechat <hdechat@gmail.com>
2018-07-26 14:24:39 -0600

    Share branch

--- src/components/stateless/Comments/Comments.js
+++ src/components/stateless/Comments/Comments.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+import { postComment } from "../../actions/actions";
+import { sendCommentToDB } from "../../api/api";
+
+
+
+
+
+
+
+
 
 const Comments = ({comments}) => {
   const allComments = comments.map((comment, index) => <p key={`key${index}`}>{comment.comment}</p>);
