fe3806249273a65262cc626a406c1443fe6ec0bb
Helen Dechat <hdechat@gmail.com>
2018-07-28 14:11:37 -0600

    Add rendered artwork to CollectionPageComponent

--- src/components/stateless/CollectionPage/CollectionPage.js
+++ src/components/stateless/CollectionPage/CollectionPage.js
@@ -1,9 +1,32 @@
 import React from "react";
-// import "./CollectionPage.css";
+import { connect } from "react-redux";
 
 const CollectionPage = (props) => {
-  console.log(props);
-  return <div>asdfsadf</div>
+  const category = props.match.params.category
+  console.log(props)
+  const artworks = props.collections[category].map(art => {
+    const { id, artist, title, year, image_link, page_link } = art;
+    return (
+      <li id={id}>
+        <img src={image_link} alt={`image of ${title}`} />
+        <a href={page_link}>link to museum page</a>
+        <p>{title}</p>
+        <p>{artist}</p>
+        <p>{year}</p>
+      </li>
+    )
+  });
+
+  return (
+    <ul>
+      {artworks}
+    </ul>
+  )
 };
 
-export default CollectionPage;
+export const mapStateToProps = (state) => ({
+  collections: state.collections
+});
+
+
+export default connect(mapStateToProps)(CollectionPage);
