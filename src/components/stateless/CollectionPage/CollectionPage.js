462a9f1d8fcf0adabc205cf52285960f853d378d
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-28 23:49:12 -0600

    Add CSS styles, HTML structure

--- src/components/stateless/CollectionPage/CollectionPage.js
+++ src/components/stateless/CollectionPage/CollectionPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./CollectionPage.css";
 
 export class CollectionPage extends Component {
 
@@ -10,19 +11,32 @@ export class CollectionPage extends Component {
     const artworks = this.props.collections[category].map(art => {
       const {id, artist, title, year, image_link, page_link} = art;
       return (
-        <li id={id}>
-          <img src={image_link} alt={`${title}`} />
-          <a href={page_link}>link to museum page</a>
-          <p>{title}</p>
-          <p>{artist}</p>
-          <p>{year}</p>
+        <li className="artwork-item" id={id}>
+          <section className="artwork-top">
+            <a className="art-museum-link" href={page_link}>Back to Museum Page</a>
+            <img src={image_link} alt={`${title}`} />
+            <section className="artwork-info">
+              <span className="artwork-artist-tag">artist</span>
+              <p className="artwork-artist">{artist}</p>
+              <p className="artwork-title">{title}</p>
+              <p className="artwork-year">{year}</p>
+            </section>
+          </section>
+          {/*<section className="artwork-item-bottom">*/}
+            {/*<a href={page_link}>Back to Museum Page</a>*/}
+          {/*</section>*/}
         </li>
       );
     });
+
     return (
-      <ul>
-        {artworks}
-      </ul>
+      <section className="collection-page-container">
+        <h2>{category}</h2>
+        <p>({this.props.collections[category].length}) Pieces</p>
+        <ul>
+          {artworks}
+        </ul>
+      </section>
     );
   };
 
