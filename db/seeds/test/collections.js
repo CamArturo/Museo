3c6b52a52cf38e463d1b74773447981ff1c5a351
Helen Dechat <hdechat@gmail.com>
2018-07-30 21:09:36 -0600

    Add comments to seed test file

--- db/seeds/test/collections.js
+++ db/seeds/test/collections.js
@@ -6,7 +6,8 @@ const collection =
       "page_link": "https://denverartmuseum.org/object/2006.218",
       "title": "Horse with Flowers Drinking Glass from the Table Stories Dinnerware",
       "year": "2005",
-      "category": "architecture design and graphics"
+      "category": "architecture design and graphics",
+      "comments": [{author_id: 1, comment: "That's a glass!"},{author_id: 2, comment: "I'd drink from that."}]
     },
     {
       "artist": "India, southern",
@@ -14,7 +15,8 @@ const collection =
       "page_link": "https://denverartmuseum.org/object/1991.1012",
       "title": "Monkey God (Hanuman)",
       "year": "19th Century",
-      "category": "asian"
+      "category": "asian",
+      "comments": [{author_id: 3, comment: "I dig it."}]
     }
   ];
 
@@ -32,15 +34,10 @@ exports.seed = function (knex, Promise) {
           image_link: artWork.image_link
         }, "id")
           .then(id => {
-            if (artWork.comments) {
-              const unresolvedPostPromises = artWork.comments.map(comment => {
-                return knex("posts").insert({
-                  comment,
-                  artwork_id: id[0]
-                });
-              });
-              return Promise.all(unresolvedPostPromises);
-            }
+            const unresolvedPostPromises = artWork.comments.map(comment => {
+                return knex("posts").insert(Object.assign(comment, {artwork_id: id[0]}));
+            });
+            return Promise.all(unresolvedPostPromises);
           })
           .catch(error => console.log(`OOPS!: ${error}`));
       });
