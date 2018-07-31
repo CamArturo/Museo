8040566124b3ac9dc2940c8287087c3de76791cf
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-30 19:06:46 -0600

    Add travis file

--- db/seeds/test/collections.js
+++ db/seeds/test/collections.js
@@ -1,22 +1,22 @@
-const collection = 
-[
-  {
-    "artist":"Tord Boontje",
-    "image_link":"https://s3.amazonaws.com/damcollections/68bd2286_8890/2000/2000_medium.jpg",
-    "page_link":"https://denverartmuseum.org/object/2006.218",
-    "title":"Horse with Flowers Drinking Glass from the Table Stories Dinnerware",
-    "year":"2005",
-    "category":"architecture design and graphics"
-  },
-  {
-    "artist":"India, southern",
-    "image_link":"https://s3.amazonaws.com/damcollections/85422afb_509/2000/2000_medium.jpg",
-    "page_link":"https://denverartmuseum.org/object/1991.1012",
-    "title":"Monkey God (Hanuman)",
-    "year":"19th Century",
-    "category":"asian"
-  }
-]
+const collection =
+  [
+    {
+      "artist": "Tord Boontje",
+      "image_link": "https://s3.amazonaws.com/damcollections/68bd2286_8890/2000/2000_medium.jpg",
+      "page_link": "https://denverartmuseum.org/object/2006.218",
+      "title": "Horse with Flowers Drinking Glass from the Table Stories Dinnerware",
+      "year": "2005",
+      "category": "architecture design and graphics"
+    },
+    {
+      "artist": "India, southern",
+      "image_link": "https://s3.amazonaws.com/damcollections/85422afb_509/2000/2000_medium.jpg",
+      "page_link": "https://denverartmuseum.org/object/1991.1012",
+      "title": "Monkey God (Hanuman)",
+      "year": "19th Century",
+      "category": "asian"
+    }
+  ];
 
 exports.seed = function (knex, Promise) {
   return knex("posts").del()
